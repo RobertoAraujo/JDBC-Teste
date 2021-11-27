13dee38b121c1a422c45bd972fcef79fa5e51a9d
Roberto Araujo de Moraes <endriosrobert@gmail.com>
2021-11-27 12:03:28 -0500

    first commit

---
+++ src/main/java/br/gov/ac/sefaz/JdbcAplication.java
@@ -0,0 +1,18 @@
+package br.gov.ac.sefaz;
+
+public class JdbcAplication {
+
+//    spring.datasource.driverClassName=org.postgresql.Driver
+//    spring.datasource.url=jdbc:postgresql://localhost:5432/my_db
+//    spring.datasource.username=postgres
+//    spring.datasource.password=postgres
+//
+//    spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect
+//    spring.jpa.generate-ddl=true
+//    spring.jpa.show-sql=true
+//    spring.jpa.hibernate.ddl-auto=create
+
+    public static void main(String[] args) {
+        System.out.println("Aprendendo o JDBC");
+    }
+}
---
+++ src/main/java/br/gov/ac/sefaz/factory/JdbcFActory.java
@@ -0,0 +1,8 @@
+package br.gov.ac.sefaz.factory;
+
+public class JdbcFActory {
+    private static final String USUARIO_DB = "root";
+    private static final String BSENHA_D = "";
+    private static final String URL_DB = "jdbc:postgresql://localhost:25432/sabado";
+    private static final String DRIVER_CLASS = "org.postgresql.Driver"
+}
